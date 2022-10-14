86270bd8aff338938d26b42d2fa15cd5070350ff
Ruby <ruby0322@ntu.im>
2022-10-14 14:44:51 +0800

    HW4 Basic & Advanced Done

--- hw4/src/util/reveal.js
+++ hw4/src/util/reveal.js
@@ -1,34 +1,23 @@
+const DIRECTIONS = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, -1], [1, -1], [-1, 1]];
+
 export const revealed = (board, x, y, newNonMinesCount) => {
   board[x][y].revealed = true;
   if (board[x][y].value === '💣') return { board, newNonMinesCount };
   newNonMinesCount--;
-  // Advanced TODO: reveal cells in a more intellectual way.
-  // Useful Hint: If the cell is already revealed, do nothing.
-  //              If the value of the cell is not 0, only show the cell value.
-  //              If the value of the cell is 0, we should try to find the value of adjacent cells until the value we found is not 0.
-  //              The input variables 'newNonMinesCount' and 'board' may be changed in this function.
   if (board[x][y].value === 0) {
-    if (x+1 < board.length && board[x][y].value !== '💣' && !board[x+1][y].revealed) {
-      const res = revealed(board, x+1, y, newNonMinesCount);
-      board = res.board;
-      newNonMinesCount = res.newNonMinesCount;
-    }
-    if (x-1 >= 0 && board[x][y].value !== '💣' && !board[x-1][y].revealed) {
-      const res = revealed(board, x-1, y, newNonMinesCount);
-      board = res.board;
-      newNonMinesCount = res.newNonMinesCount;
-    }
-    if (y+1 < board[x].length && board[x][y].value !== '💣' && !board[x][y+1].revealed) {
-      const res = revealed(board, x, y+1, newNonMinesCount);
-      board = res.board;
-      newNonMinesCount = res.newNonMinesCount;
-    }
-    if (y-1 >= 0 && board[x][y].value !== '💣' && !board[x][y-1].revealed) {
-      const res = revealed(board, x, y-1, newNonMinesCount);
-      board = res.board;
-      newNonMinesCount = res.newNonMinesCount;
-    }
+    /* DFS */
+    DIRECTIONS.map(
+      ([dx, dy]) => {
+        const newX = x + dx;
+        const newY = y + dy;
+        if (0 <= newX && newX < board.length && 0 <= newY && newY < board.length && !board[newX][newY].revealed) {
+          const res = revealed(board, newX, newY, newNonMinesCount);
+          board = res.board;
+          newNonMinesCount = res.newNonMinesCount;
+        }
+        return undefined;
+      }
+    );
   }
-  
   return { board, newNonMinesCount };
 };
